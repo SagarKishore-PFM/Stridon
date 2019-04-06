ff790cbe4871aa95f528cc181b2a92276dd36e13
Sagar Kishore <sagarkishore.ckl@gmail.com>
2019-04-06 23:39:41 +0530

    Removed unncessary fields in Article model. Some linting fixes

--- stridon_app/models.py
+++ stridon_app/models.py
@@ -4,12 +4,13 @@ from django.contrib.auth.models import User
 
 class Article(models.Model):
     title = models.CharField(max_length=100, unique=True)
-    author = models.ForeignKey(User, related_name='articles', on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User,
+        related_name='articles',
+        on_delete=models.CASCADE
+    )
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
-    last_updated = models.DateTimeField(auto_now=True, null=True)
-    updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.CASCADE)
-    publish = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
