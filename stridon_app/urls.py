b4f32026bc79605a1a5698aa5bbe7ff2d7387f58
Sagar Kishore <sagarkishore.ckl@gmail.com>
2019-04-06 21:29:43 +0530

    Moved login page to the right directory. Added url for subscribe

--- stridon_app/urls.py
+++ stridon_app/urls.py
@@ -6,8 +6,9 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='stridon_app/login.html'), name='login'),
     path('signup/', views.signup, name='signup'),
     path('alice/', views.alice, name='alice'),
     path('doctor/', views.doctor, name='doctor'),
+    path('subscribe/', views.subscribe, name='subscribe'),
 ]
