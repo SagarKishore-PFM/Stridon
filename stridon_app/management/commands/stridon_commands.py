13babdafed45cf3c66d2b938fafcee6872250da5
Sagar Kishore <sagarkishore.ckl@gmail.com>
2019-04-06 19:59:37 +0530

    Renamed the file.

---
+++ stridon_app/management/commands/stridon_commands.py
@@ -0,0 +1,47 @@
+from django.core.management import BaseCommand
+
+from django.contrib.auth.models import Group, Permission, User
+from django.contrib.contenttypes.models import ContentType
+from ...models import Article
+
+
+class Command(BaseCommand):
+    # Show this when the user types help
+    help = "Create Test Users"
+
+    # A command must define handle()
+    def handle(self, *args, **options):
+        # Create Test Users
+        self.stdout.write("Creating test Users")
+        alice1, created = User.objects.create_user(
+            'alice1',
+            password='stridontest123',
+            )
+        alice2, created = User.objects.get_or_create(
+            'alice2',
+            password='stridontest456',
+            )
+        alice1.save()
+        alice2.save()
+
+        self.stdout.write("Test Users Created Successfully")
+
+        # Create Groups
+
+        self.stdout.write("Creating Free and Paid Groups")
+
+
+        # free_user_group, created = Group.objects.get_or_create(name='free_users_group')
+
+        # # Get Content for Article Model
+# paid_user_group, created = Group.objects.get_or_create(name='Paid Users Group')
+# ct = ContentType.objects.get_for_model(Group)
+# can_view_paid_articles_permission = Permission.objects.create(
+#     codename='can_view_paid_articles',
+#     name='Can view Paid Articles',
+#     content_type=ct)
+# paid_user_group.permissions.add(can_view_paid_articles_permission)
+
+
+
+# Add them to groups
