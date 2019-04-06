f186d8259ba5329f94e67e0905e0a508c13c6401
Sagar Kishore <sagarkishore.ckl@gmail.com>
2019-04-06 23:40:29 +0530

    Linting fixes. Added urls for add and view articles

--- stridon_app/urls.py
+++ stridon_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -6,10 +6,22 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('login/', auth_views.LoginView.as_view(template_name='stridon_app/login.html'), name='login'),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name='stridon_app/login.html'
+        ),
+        name='login'
+    ),
     path('signup/', views.signup, name='signup'),
     path('alice/', views.alice, name='alice'),
     path('doctor/', views.doctor, name='doctor'),
     path('subscribe/', views.subscribe, name='subscribe'),
     path('unsubscribe/', views.unsubscribe, name='unsubscribe'),
+    path('add_article/', views.add_article, name='add-article'),
+    re_path(
+        r'^view_article/(?P<article_id>\d+)/$',
+        views.view_article,
+        name='view-article'
+    ),
 ]
