58e16c7fd57f58242199a5561544d21f7af3915a
Sagar Kishore <sagarkishore.ckl@gmail.com>
2019-04-06 20:41:17 +0530

    changed the name. fixed user creation bug. added code to generate groups and add users to groups and check their permissions.

--- stridon_app/management/commands/stridon_commands.py
+++ stridon_app/management/commands/stridon_commands.py
@@ -1,6 +1,7 @@
 from django.core.management import BaseCommand
 
 from django.contrib.auth.models import Group, Permission, User
+
 from django.contrib.contenttypes.models import ContentType
 from ...models import Article
 
@@ -13,13 +14,23 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # Create Test Users
         self.stdout.write("Creating test Users")
-        alice1, created = User.objects.create_user(
-            'alice1',
-            password='stridontest123',
+        try:
+            alice1 = User.objects.get(username='alice1')
+            self.stdout.write("Alice1 exists")
+        except User.DoesNotExist:
+            self.stdout.write("Alice1 does not exist...creating")
+            alice1 = User.objects.create_user(
+                'alice1',
+                password='stridontest123',
             )
-        alice2, created = User.objects.get_or_create(
-            'alice2',
-            password='stridontest456',
+        try:
+            alice2 = User.objects.get(username='alice2')
+            self.stdout.write("Alice2 exists")
+        except User.DoesNotExist:
+            self.stdout.write("Alice2 does not exist...creating")
+            alice2 = User.objects.create_user(
+                'alice2',
+                password='stridontest123',
             )
         alice1.save()
         alice2.save()
@@ -27,20 +38,31 @@ class Command(BaseCommand):
         self.stdout.write("Test Users Created Successfully")
 
         # Create Groups
-
         self.stdout.write("Creating Free and Paid Groups")
+        free_user_group, created = Group.objects.get_or_create(name='free_users_group')
+        paid_user_group, created = Group.objects.get_or_create(name='Paid Users Group')
 
+        # Fetch the required permission and add it to the group
+        self.stdout.write("Adding permissions to Paid Groups")
+        can_view_paid_articles_permission = Permission.objects.get(
+            codename='can_view_paid_articles',
+        )
+        paid_user_group.permissions.add(can_view_paid_articles_permission)
+        paid_user_group.save()
+        free_user_group.save()
 
-        # free_user_group, created = Group.objects.get_or_create(name='free_users_group')
+        # Add users to the groups
+        self.stdout.write("Adding users to Free and Paid Groups")
+        paid_user_group.user_set.add(alice1)
+        free_user_group.user_set.add(alice2)
 
-        # # Get Content for Article Model
-# paid_user_group, created = Group.objects.get_or_create(name='Paid Users Group')
-# ct = ContentType.objects.get_for_model(Group)
-# can_view_paid_articles_permission = Permission.objects.create(
-#     codename='can_view_paid_articles',
-#     name='Can view Paid Articles',
-#     content_type=ct)
-# paid_user_group.permissions.add(can_view_paid_articles_permission)
+        # Assert users have permissions
+        self.stdout.write("Asserting permissions for each user")
+        if alice1.has_perm('stridon_app.can_view_paid_articles'):
+            self.stdout.write("Alice 1 pass")
+        if not alice2.has_perm('stridon_app.can_view_paid_articles'):
+            self.stdout.write("Alice 2 pass")
+
 
 
 
