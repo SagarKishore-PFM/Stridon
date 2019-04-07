3fce53141910f6d414d15772e59b77f53df532ad
Sagar Kishore <sagarkishore.ckl@gmail.com>
2019-04-07 11:54:31 +0530

    Added a new field called premium content to the Article model

--- stridon_app/models.py
+++ stridon_app/models.py
@@ -11,6 +11,7 @@ class Article(models.Model):
     )
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
+    premium_content = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
